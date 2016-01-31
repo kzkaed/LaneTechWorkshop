0038b02825255a5699e8b1ff71975a1384875687
kzkaed <tree@scutigera.com>
2016-01-31 17:55:18 -0600

    Update across workshop files

--- shapes.js
+++ shapes.js
@@ -14,8 +14,15 @@ function showElement(elementName, id) {
   document.getElementById(elementName + id).style.visibility = 'visible';
 }
 
-function setVisibilityOfElement (elementName, id, visibilityState) {
+function setVisibilityId (elementName, id, visibilityState) {
   document.getElementById(elementName + id).style.visibility = visibilityState;
+}
+
+function setVisibiltiyClass(className, visibilityState) {
+  var elements = document.getElementsByClassName(className);
+  for (var i = 0; i < elements.length; i++){
+  	elements[i].style.visibility = visibilityState;
+  }
 }
 
 function hideAllShapes(start, end) {
